9c35308756c1ce736c67ec6db6b1d99f10fc3810
Lavinya Aparecida Padua Tomazolli <13300905605@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-15 11:28:50 -0300

    aula sobre operadores logicos

---
+++ aula4/operadores_logicos.php
@@ -0,0 +1,19 @@
+<h2>operadores logicos</h2>
+
+<ol>
+    <li>and - E</li>
+    <li>or - OU</li>
+    <li>xor - Ou exclusivo</li>
+    <li>! - Não</li>
+    <li>&& - E</li>
+    <li>|| - OU</li>
+</ol>
+
+<?php
+    $media = 6;
+    $faltas = 20;
+    $criterio1 = $media > 6; //false
+    $creterio2 = $faltas < 25; //true
+    $resultado = $critorio1 && $creterio2; //false
+    var_dump ($resultado)
+?>
